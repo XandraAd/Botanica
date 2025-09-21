98f90fb538fba6e1a998e08b30fd770aa2713d7b
XandraAd <ewuramaaddo@gmail.com>
2025-09-21 09:10:20 +0000

    updated Admin Route to fix bug(dashboard loading before home page)

--- frontend/src/pages/Admin/AdminRoute.jsx
+++ frontend/src/pages/Admin/AdminRoute.jsx
@@ -3,12 +3,11 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const AdminRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return userInfo && userInfo.isAdmin ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" replace />
-  );
+  const { userInfo, authReady } = useSelector((state) => state.auth);
+
+  if (!authReady) return null;
+
+  return userInfo && userInfo.isAdmin ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default AdminRoute;
